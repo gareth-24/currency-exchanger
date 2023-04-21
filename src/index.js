6db4baa675aa7851c79a23b6429d181889a413c1
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-21 16:45:16 -0700

    WIP: trying to get request for two inputted currencies.

--- src/index.js
+++ src/index.js
@@ -4,24 +4,43 @@ import './css/styles.css';
 import ExchangeRateService from './services/exchange-rate-service';
 
 //Business Logic
-
-async function getConversionRates(dollarsInput,currencyInput)  {
+//WIP: not getting a response from the API with the new request URL
+async function getConversionRates(amountInput,initialCurrency,foreignCurrency)  {
   const response = await ExchangeRateService.getConversionRates();
-  const currencyId = currencyInput.toUpperCase();
-  const dollars = parseInt(Math.abs(dollarsInput));
+  const currency1Id = initialCurrency.toUpperCase();
+  const currency2Id = foreignCurrency.toUpperCase();
+  const dollars = parseInt(Math.abs(amountInput));
   checkValidUsdInput(dollars);
-  console.log(dollars,currencyId);
-  // console.log(typeof dollars);
-  if (response.conversion_rates && (checkValidCurrencyId(currencyId,response)===true))  {
+
+  if (response.conversion_rates)  {
     //round converted amount to 2 decimals
-    const convertedAmount = Math.round(dollars*(response.conversion_rates[currencyId])*100)/100;
-    printElements(response,convertedAmount,currencyId);
-  } else if (response.conversion_rates && checkValidCurrencyId(currencyId,response)===false) {
-    printInputError();
+    // const convertedAmount = Math.round(dollars*(response.conversion_rates[currencyId])*100)/100;
+    const convertedAmount = response.conversion_result
+    printElements(response,convertedAmount,currency1Id,currency2Id);
+  // } else if (response.conversion_rates && checkValidCurrencyId(currencyId,response)===false) {
+  //   printInputError();
   } else  {
-    printError(response,currencyId);
+    printError(response,currency1Id,currency2Id);
   }
 }
+
+
+// async function getConversionRates(dollarsInput,currencyInput,)  {
+//   const response = await ExchangeRateService.getConversionRates();
+//   const currencyId = currencyInput.toUpperCase();
+//   const dollars = parseInt(Math.abs(dollarsInput));
+//   checkValidUsdInput(dollars);
+
+//   if (response.conversion_rates && (checkValidCurrencyId(currencyId,response)===true))  {
+//     //round converted amount to 2 decimals
+//     const convertedAmount = Math.round(dollars*(response.conversion_rates[currencyId])*100)/100;
+//     printElements(response,convertedAmount,currencyId);
+//   } else if (response.conversion_rates && checkValidCurrencyId(currencyId,response)===false) {
+//     printInputError();
+//   } else  {
+//     printError(response,currencyId);
+//   }
+// }
 
 function checkValidUsdInput(dollarsInput)  {
   //if the user does not enter a valid input, return the base conversion rate for 1$
@@ -32,45 +51,42 @@ function checkValidUsdInput(dollarsInput)  {
   }
 }
 
-function checkValidCurrencyId(currencyId,response)  {
-  //checks if the entered currency type matches a key-value from the API
-  const validCurrencyIds = Object.keys(response.conversion_rates);
-  // console.log(validCurrencyIds);
-  if (validCurrencyIds.includes(currencyId))  {
-    // console.log("valid currency");
-    return true;
-  } else {
-    // console.log("not a valid currency");
-    return false;
-  }
-}
+// function checkValidCurrencyId(currencyId,response)  {
+//   //checks if the entered currency type matches a key-value from the API
+//   const validCurrencyIds = Object.keys(response.conversion_rates);
+
+//   if (validCurrencyIds.includes(currencyId))  {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
 
 //User Interface Logic
 
-function printElements(response,convertedAmount,currencyId)  {
-  document.querySelector("#showResponse").innerText = `The current exchange rate from USD to ${currencyId} is ${response.conversion_rates[currencyId]}.`;
-  document.querySelector("#convertedAmount").innerText = `Converted Value: ${convertedAmount} ${currencyId}`;
+function printElements(response,convertedAmount,currency1Id,currency2Id)  {
+  document.querySelector("#showResponse").innerText = `The current exchange rate from ${currency1Id} to ${currency2Id} is ${response.conversion_rates}.`;
+  document.querySelector("#convertedAmount").innerText = `Converted Value: ${convertedAmount} ${currency2Id}`;
 }
 
-function printError(error,currencyId) {
-  document.querySelector('#showResponse').innerText = `Sorry, there was an error accessing the currency exchange rate for ${currencyId}: 
+function printError(error,currency1Id,currency2Id) {
+  document.querySelector('#showResponse').innerText = `Sorry, there was an error accessing the currency exchange rate for ${currency1Id} to ${currency2Id}: 
   ${error}.`;
-  // console.log("printError has been triggered");
 }
 
-function printInputError()  {
-  document.querySelector("#showResponse").innerText = "Error: the currency you have entered does not exist. Please enter a valid currency";
-}
+// function printInputError()  {
+//   document.querySelector("#showResponse").innerText = "Error: the currency you have entered does not exist. Please enter a valid currency";
+// }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const dollars = document.querySelector("#input-dollars").value;
-  const currencyId = document.querySelector("#input-convert-to").value;
-  // document.querySelector("#input-dollars").value = null;
-  // document.querySelector("#input-convert-to").value = null;
+  const amount = parseInt(document.querySelector("#input-dollars").value);
+  const resultingCurrency = document.querySelector("#input-convert-to").value;
+  const startingCurrency = document.querySelector("#input-convert-from").value;
+  console.log(amount,resultingCurrency,startingCurrency);
   document.querySelector("#showResponse").innerText = null;
   document.querySelector("#convertedAmount").innerText = null;
-  getConversionRates(dollars,currencyId);
+  getConversionRates(amount,startingCurrency,resultingCurrency);
 }
 
 window.addEventListener("load", function() {
