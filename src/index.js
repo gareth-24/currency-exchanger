d33b072e10051b2b11522462fba0b4e642bde370
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-21 09:12:34 -0700

    WIP: logic to call API and receive submit form.

--- src/index.js
+++ src/index.js
@@ -5,12 +5,32 @@ import ExchangeRateService from './services/exchange-rate-service';
 
 //Business Logic
 
+async function getConvertedCurrency(dollars,currencyId)  {
+  const response = await ExchangeRateService.getConvertedCurrency();
+  console.log(response.conversion_rates.CAD);
+  console.log(dollars,currencyId);
+  if (response.conversion_rates)  {
+    const convertedAmount = dollars*(response.conversion_rates.currencyId);
+    printElements(response,convertedAmount);
+  } else  {
+    printError(response);
+  }
+}
 
 //User Interface Logic
 
+function printElements(response)  {
+  document.querySelector('showResponse').innerText = `The exchange rate is ${response.exchange_rate.CAD}. Converted amount: ${convertedAmount}`;
+}
+
+function printError(error) {
+  document.querySelector('#showResponse').innerText = `There was an error accessing the currency exchange rate data: 
+  ${error}.`;
+}
+
 function handleFormSubmission(event) {
   event.preventDefault();
-
+  //WIP get inputs from submit form and call conversion rate functions
 }
 
 window.addEventListener("load", function() {
--- src/services/exchange-rate-service.js
+++ src/services/exchange-rate-service.js
@@ -1,3 +1,16 @@
 export default class ExchangeRateService  {
-  
+  static async getConvertedCurrency() { //no argument needed, API returns a list of conversion rates
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        //call an error message
+        throw new Error(`${response.status} ${response.statusText} 
+        ${jsonifiedResponse.message}`);
+      }
+      return jsonifiedResponse;
+    } catch(error)  {
+      return error;
+    }
+  }
 }
