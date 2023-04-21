a3d874181df75e0e365d345819bc0ca31d819954
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-21 12:03:07 -0700

    add branching logic to account for user input errors.

--- src/index.js
+++ src/index.js
@@ -8,12 +8,35 @@ import ExchangeRateService from './services/exchange-rate-service';
 async function getConversionRates(dollars,currencyInput)  {
   const response = await ExchangeRateService.getConversionRates();
   const currencyId = currencyInput.toUpperCase();
+  checkValidUsdInput(dollars);
+  // checkValidCurrencyId(currencyId,response);
   // console.log(dollars,currencyId);
-  if (response.conversion_rates)  {
+  if (response.conversion_rates && (checkValidCurrencyId(currencyId,response)===true))  {
     const convertedAmount = dollars*(response.conversion_rates[currencyId]);
     printElements(response,convertedAmount,currencyId);
+  } else if (response.conversion_rates && checkValidCurrencyId(currencyId,response)===false) {
+    printInputError();
   } else  {
     printError(response,currencyId);
+  }
+}
+
+function checkValidUsdInput(dollarsInput)  {
+  //if the user does not enter a valid input, return the base conversion rate for 1$
+  if (isNaN(dollarsInput)) {
+    dollarsInput = 1;
+  }
+}
+
+function checkValidCurrencyId(currencyId,response)  {
+  const validCurrencyIds = Object.keys(response.conversion_rates);
+  // console.log(validCurrencyIds);
+  if (validCurrencyIds.includes(currencyId))  {
+    // console.log("valid currency");
+    return true;
+  } else {
+    // console.log("not a valid currency");
+    return false;
   }
 }
 
@@ -27,6 +50,10 @@ function printError(error,currencyId) {
   document.querySelector('#showResponse').innerText = `Sorry, there was an error accessing the currency exchange rate for ${currencyId}: 
   ${error}.`;
   // console.log("printError has been triggered");
+}
+
+function printInputError()  {
+  document.querySelector("#showResponse").innerText = "Error: the currency you have entered does not exist. Please enter a valid currency";
 }
 
 function handleFormSubmission(event) {
