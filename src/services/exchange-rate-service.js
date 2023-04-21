978217006bc3fdffdf05cb85c32bd1d6d9d75a53
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-21 16:51:11 -0700

    Bugfix: CORS error, this will revert back to previous functionality.

--- src/services/exchange-rate-service.js
+++ src/services/exchange-rate-service.js
@@ -1,7 +1,7 @@
 export default class ExchangeRateService  {
-  static async getConversionRates(amountInput,initialCurrency,foreignCurrency) {
+  static async getConversionRates() {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${amountInput}/${initialCurrency}/${foreignCurrency}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`);
       const jsonifiedResponse = await response.json();
       if (!response.ok) {
         const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
