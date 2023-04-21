aa66513b22ea989cef1154be2b54ab472ded50e7
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-21 08:44:22 -0700

    add initial exchange rate service file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import ExchangeRateService from './services/exchange-rate-service';
 
 //Business Logic
 
 
 //User Interface Logic
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+}
+
+window.addEventListener("load", function() {
+  document.querySelector('form').addEventListener("submit", handleFormSubmission);
+});
